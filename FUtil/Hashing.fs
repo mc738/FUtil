0f926161b37a632546cf528730b3e6191dc8cae0
max <me@mclifford.dev>
2020-10-23 22:28:53 +0100

    Fixed errors

--- FUtil/Hashing.fs
+++ FUtil/Hashing.fs
@@ -9,43 +9,43 @@ let private computeHash name (data:byte array) =
 let md5 data = computeHash "MD5" data
 
 /// Get the `MD5` hash of a byte array, as a hex string.
-let md5Hex data = ConversionHelpers.bytesToHex (md5 data)    
+let md5Hex data = Conversions.bytesToHex (md5 data)    
 
 /// Get the `md5` hash of a byte array, as a base64 string.        
-let sh1Base64 data = ConversionHelpers.bytesToBase64 (md5 data)
+let sh1Base64 data = Conversions.bytesToBase64 (md5 data)
 
 /// Get the `SHA1` hash of a byte array, as a bytes.    
 let sha1 data = computeHash "SHA1" data
 
 /// Get the `SHA1` hash of a byte array, as a hex string.
-let sha1Hex data = ConversionHelpers.bytesToHex (sha1 data)    
+let sha1Hex data = Conversions.bytesToHex (sha1 data)    
 
 /// Get the `SHA1` hash of a byte array, as a base64 string.        
-let sha1Base64 data = ConversionHelpers.bytesToBase64 (sha1 data)
+let sha1Base64 data = Conversions.bytesToBase64 (sha1 data)
 
 /// Get the `SHA256` hash of a byte array, as a bytes.
 let sha256 data = computeHash "SHA256" data
 
 /// Get the `SHA256` hash of a byte array, as a hex string.
-let sha256Hex data = ConversionHelpers.bytesToHex (sha256 data)    
+let sha256Hex data = Conversions.bytesToHex (sha256 data)    
 
 /// Get the `SHA256` hash of a byte array, as a base64 string.        
-let sh256Base64 data = ConversionHelpers.bytesToBase64 (sha256 data)
+let sh256Base64 data = Conversions.bytesToBase64 (sha256 data)
 
 /// Get the `SHA384` hash of a byte array, as a bytes.
 let sha384 data = computeHash "SHA384" data
 
 /// Get the `SHA384` hash of a byte array, as a hex string.
-let sha384Hex data = ConversionHelpers.bytesToHex (sha384 data)    
+let sha384Hex data = Conversions.bytesToHex (sha384 data)    
 
 /// Get the `SHA384` hash of a byte array, as a base64 string.        
-let sh384Base64 data = ConversionHelpers.bytesToBase64 (sha384 data)
+let sh384Base64 data = Conversions.bytesToBase64 (sha384 data)
 
 /// Get the `SHA512` hash of a byte array, as a bytes.
 let sha512 data = computeHash "SHA512" data
 
 /// Get the `SHA512` hash of a byte array, as a hex string.
-let sha512Hex data = ConversionHelpers.bytesToHex (sha512 data)    
+let sha512Hex data = Conversions.bytesToHex (sha512 data)    
 
 /// Get the `SHA512` hash of a byte array, as a base64 string.        
-let sh512Base64 data = ConversionHelpers.bytesToBase64 (sha512 data)
+let sh512Base64 data = Conversions.bytesToBase64 (sha512 data)
